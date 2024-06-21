3e9eef434c64864b0f9941aa2f0b263ea5c05869
chrissda <jhersonbarreto88@hotmail.com>
2024-06-20 21:36:49 -0500

    avence curso

--- Semana11/01-ecommerce/src/views/LoginView.jsx
+++ Semana11/01-ecommerce/src/views/LoginView.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/authContext";
+import FormUser from "../components/FormUser";
 
 const LoginView = () => {
   const { LoginWithGoogle } = useContext(AuthContext);
@@ -13,13 +14,21 @@ const LoginView = () => {
     }
   }
 
+  const handleLoginEmail = (email, password) => {
+    console.log("handleLoginEmail", email, password)
+  }
+  const handleRegisterEmail = (email, password) => {
+    console.log("handleRegisterEmail", email, password)
+  }
+
   return (
-    <div className="container min-h-96 flex justify-center items-center">
-      <button className="px-5 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg flex items-center text-lg" onClick={handleLogin}>
-        Iniciar sesión con <i className="fa-brands fa-google-plus-g me-3 fa-2x"></i>
+    <div className="container min-h-96 flex justify-center items-center flex-col">
+      <FormUser handleLoginEmail={ handleLoginEmail } handleRegisterEmail={ handleRegisterEmail } />
+      <button className="px-5 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded-lg flex items-center text-lg w-96" onClick={handleLogin}>
+        <i className="fa-brands fa-google-plus-g me-3 fa-2x"></i> Ingresa con Google
       </button>
     </div>
-  )
-}
+  );
+};
 
 export default LoginView
